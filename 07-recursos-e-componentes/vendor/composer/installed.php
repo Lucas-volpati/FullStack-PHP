f2c7f58004d22c94a012149c7e59901cad3a4485
Lucas <lcs.rodrigues16@hotmail.com>
2022-03-22 13:22:26 -0300

    gerenciando dependencias

---
+++ 07-recursos-e-componentes/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e860a9fbe5bdef235a5c6c75f5714e4948e28f75',
+        'name' => 'upinside/fsphp',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'upinside/fsphp' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e860a9fbe5bdef235a5c6c75f5714e4948e28f75',
+            'dev_requirement' => false,
+        ),
+    ),
+);
